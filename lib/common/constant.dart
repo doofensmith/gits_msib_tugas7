b1a4ec39f69fb8dafd04079061ba971a3d0e3708
doofensmith <wahyuwijanarko3998@gmail.com>
2021-10-19 22:37:23 +0700

    almost done, homepage

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -1,6 +1,6 @@
 class Constant {
   static const String baseUrlPost =
-      'https://gits-msib.my.id/wp-json/wp/v2/posts?per_page=50';
+      'https://gits-msib.my.id/wp-json/wp/v2/posts';
 
   static const String baseUrlAuth =
       'https://gits-msib.my.id/wp-json/jwt-auth/v1/token';
